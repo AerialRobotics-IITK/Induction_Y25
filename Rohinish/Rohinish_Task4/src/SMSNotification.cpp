a75d352c6350cc4784ea36ab0717d19436cc9ec2
jatinagarwal08 <jatinagarwal.gwl@gmail.com>
2026-06-09 18:12:57 +0530

    Delete Rohinish directory

--- Rohinish/Rohinish_Task4/src/SMSNotification.cpp
+++
@@ -1,9 +0,0 @@
-#include "SMSNotification.h"
-
-SMSNotification::SMSNotification(const std::string& phoneNumber, const std::string& message)
-    : Notification(message), phoneNumber_(phoneNumber), deliveryStatus_("Pending") {}
-
-void SMSNotification::sendNotification() {
-    std::cout << "[SMS -> " << phoneNumber_ << "] " << message_ << std::endl;
-    deliveryStatus_ = "Sent";
-}
